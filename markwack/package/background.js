eefe626d9c03be6c2450967aa6875f525f0cea1e
Andrew Fuchs <afuchs@google.com>
2021-04-07 18:27:22 -0700

    Essentially works, as long as you reload the extension each time. Needs the re-initialization fixed for a new on-click.

--- markwack/package/background.js
+++ markwack/package/background.js
@@ -10,59 +10,45 @@ var theContent="";
 
 var currentState="none";
 
+// --- The state table. Note click in any state restarts ---
+
 var stateTable= {
   none: {
     on_click: {next: "started", func: enterStarted}
   },
   started: {
+    on_click: {next: "started", func: enterStarted},
     tab_done: {next: "hastab", func: enterHasTab},
     content_done: {next: "hascontent", func: enterHasContent}
   },
   hastab: {
+    on_click: {next: "started", func: enterStarted},
     content_done: {next: "ready", func: enterReady}
   },
   hascontent: {
+    on_click: {next: "started", func: enterStarted},
     tab_done: {next: "ready", func: enterReady}
   },
   ready: {
+    on_click: {next: "started", func: enterStarted},
     do_output: {next: "done", func: enterDone}
   },
   done: {
-    // Allow new click after cycle completes
     on_click: {next: "started", func: enterStarted}
   }
 }
 
 function handleEvent( event, data ) {
   if( event in stateTable[currentState] ) {
+    console.log( "Currently in "+currentState+" state; received event "+event);
+    let nextState=stateTable[currentState][event].next;
     stateTable[currentState][event]["func"](data);
-    currentState=stateTable[currentState][event].next;
+    currentState=nextState;
   }
   else {
-    console.log( "(ignore "+event+" not valid in state "+currentState);
+    console.log( "(ignore "+event+" not valid in state "+currentState+")");
   }
 }
-
-/*
-// alternate listener for testing state transitions
-chrome.action.onClicked.addListener((tab) => {
-  handleEvent("on_click");
-  handleEvent("content_done");
-  handleEvent("content_done");
-  handleEvent("tab_done");
-  handleEvent("tab_done");
-  handleEvent("tab_done");
-  handleEvent("do_output");
-  currentState="none";
-  handleEvent("on_click");
-  handleEvent("tab_done");
-  handleEvent("tab_done");
-  handleEvent("do_output");
-  handleEvent("on_click");
-  handleEvent("content_done");
-  handleEvent("do_output");
-});
-*/
 
 
 /*
@@ -99,35 +85,43 @@ function setUpSourceTab(tabid) {
 function enterStarted(srctab) {
   function cacheSinkTabID(tab) {
     sinkTab=tab.id;
+    console.log("  sink tab ID="+sinkTab);
   }
+  console.log( "Running enterStarted; source tab ID="+srctab.id);
   setUpSourceTab(srctab.id);         // set up the source tab
   chrome.tabs.create( {              // create the sink tab
       active: false,
-      url: "result.html"
+      url: "result.html"             // result.html invokes selfout.js
     },
     (tab) => cacheSinkTabID(tab)
   );
+}
+
+function enterHasTab(foo) {
+  // Poke the source page now. Moved here from enterStarted
+  // to provide more timing slack...
+
+  console.log( "Running enterHasTab");
   chrome.tabs.sendMessage(           // ask the source tab for content
     sourceTab,
     {op: "create", data: "" }
   );
 }
 
-function enterHasTab(foo) {
-  // no-op: this state just registers
-  // that the sink page is alive
-}
-
 function enterHasContent(content) {
+  console.log( "Running enterHasContent, with this content:" );
+  console.log( content );
   theContent = content;              // cache the content
 }
 
-function enterReady(content) {
+function enterReady(foo) {
+  console.log( "Running enterReady" );
   // send content to output tab
-  chrome.tabs.sendMessage( sinkTab, {op: "create", data: "" });
+  chrome.tabs.sendMessage( sinkTab, {op: "output", data: foo });
 }
 
 function enterDone() {
-  // activate the ouput tab
+  console.log( "Running enterDone" );
+  // activate the output tab
 }
 
